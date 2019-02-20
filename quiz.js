e0be85b4d90d1d5a3ca800372f1ef9e4691db800
KasiaWerner <kwerner@centrumcyfrowe.pl>
2019-02-20 15:38:14 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,62 +1,15 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
+(function() {
+  function buildQuiz() {
+    // we'll need a place to store the HTML output
+    const output = [];
 
-function buildQuiz(){}
-
-function showResults(){}
-
-// display quiz right away
-buildQuiz();
-
-// on submit, show results
-submitButton.addEventListener('click', showResults);
-
-const myQuestions = [
-  {
-    question: "Who is the strongest?",
-    answers: {
-      a: "Superman",
-      b: "The Terminator",
-      c: "Waluigi, obviously"
-    },
-    correctAnswer: "c"
-  },
-  {
-    question: "What is the best site ever created?",
-    answers: {
-      a: "SitePoint",
-      b: "Simple Steps Code",
-      c: "Trick question; they're both the best"
-    },
-    correctAnswer: "c"
-  },
-  {
-    question: "Where is Waldo really?",
-    answers: {
-      a: "Antarctica",
-      b: "Exploring the Pacific Ocean",
-      c: "Sitting in a tree",
-      d: "Minding his own business, so stop asking"
-    },
-    correctAnswer: "d"
-  }
-];
-
-function buildQuiz(){
-  // we'll need a place to store the HTML output
-  const output = [];
-
-  // for each question...
-  myQuestions.forEach(
-    (currentQuestion, questionNumber) => {
-
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
       // we'll want to store the list of answer choices
       const answers = [];
 
       // and for each available answer...
-      for(letter in currentQuestion.answers){
-
+      for (letter in currentQuestion.answers) {
         // ...add an HTML radio button
         answers.push(
           `<label>
@@ -70,13 +23,83 @@ function buildQuiz(){
       // add this question and its answers to the output
       output.push(
         `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
+        <div class="answers"> ${answers.join("")} </div>`
       );
+    });
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults() {
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll(".answers");
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = "lightgreen";
+      } else {
+        // if answer is wrong or blank
+        // color the answers red
+        answerContainers[questionNumber].style.color = "red";
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  const quizContainer = document.getElementById("quiz");
+  const resultsContainer = document.getElementById("results");
+  const submitButton = document.getElementById("submit");
+  const myQuestions = [
+    {
+      question: "Who is the strongest?",
+      answers: {
+        a: "Superman",
+        b: "The Terminator",
+        c: "Waluigi, obviously"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What is the best site ever created?",
+      answers: {
+        a: "SitePoint",
+        b: "Simple Steps Code",
+        c: "Trick question; they're both the best"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Where is Waldo really?",
+      answers: {
+        a: "Antarctica",
+        b: "Exploring the Pacific Ocean",
+        c: "Sitting in a tree",
+        d: "Minding his own business, so stop asking"
+      },
+      correctAnswer: "d"
     }
-  );
+  ];
 
-  // finally combine our output list into one string of HTML and put it on the page
-  quizContainer.innerHTML = output.join('');
-}
+  // display quiz right away
+  buildQuiz();
 
-
+  // on submit, show results
+  submitButton.addEventListener("click", showResults);
+})();
